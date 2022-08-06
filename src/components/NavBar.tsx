921ed877549e8951fc2980eca247aa68017f8f80
Daniel <daniel16henrrique@gmail.com>
2022-08-06 20:14:05 -0300

    Editando NavBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,3 @@
-import IconBoxes from "../icons/IconBoxes";
 import IconSpring from "../icons/IconSpring";
 
 const NavBar = () => {
@@ -6,7 +5,7 @@ const NavBar = () => {
     <nav className="navbar navbar-light bg-dark">
       <div className="container-fluid">
         <span className="navbar-brand mb-0 h1 text-white">
-          <IconSpring /> Spring Admin - Monitoramento das APIs <IconBoxes />
+          <IconSpring /> Spring Admin
         </span>
       </div>
     </nav>
