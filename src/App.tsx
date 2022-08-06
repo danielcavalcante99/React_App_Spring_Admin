473556e7dca9502f228d4db077595af94086b984
Daniel <daniel16henrrique@gmail.com>
2022-08-06 15:16:04 -0300

    Chamando Routes

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.css'
+import './assets/css/style.css';
+import RoutesPages from './routes/Routes';
+
+function App() {
+  return (
+    <div className="App">
+      <RoutesPages />
+    </div>
+  );
+}
+
+export default App;
